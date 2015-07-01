8dda5152924766f014d8be0a9d3a391430abbfcd
yanlong <ylyanlongm@gmail.com>
2015-07-01 21:22:44 +0800

    测试提交

---
+++ src/cn/com/yl/string/MyStringPrac1.java
@@ -0,0 +1,11 @@
+package cn.com.yl.string;
+
+public class MyStringPrac1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "123456789";
+		System.out.println(str1.substring(3, 7) );
+	}
+
+}
