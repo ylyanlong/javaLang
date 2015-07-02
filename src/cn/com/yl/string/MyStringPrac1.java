683b86466577eb34041f25429ad5e4897adbea69
yanlong <ylyanlongm@gmail.com>
2015-07-02 10:51:24 +0800

    add second commit

--- src/cn/com/yl/string/MyStringPrac1.java
+++ src/cn/com/yl/string/MyStringPrac1.java
@@ -4,7 +4,7 @@ public class MyStringPrac1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str1 = "123456789";  // commit from laptop
+		String str1 = "123456789";  // commit from laptop  add second commit
 		System.out.println(str1.substring(3, 7) );  // ע��
 	}
 
