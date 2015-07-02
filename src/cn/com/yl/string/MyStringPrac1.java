a55501c2785be674f823605b6b2c08291f196074
yanlong <ylyanlongm@gmail.com>
2015-07-02 09:16:41 +0800

    添加注释

--- src/cn/com/yl/string/MyStringPrac1.java
+++ src/cn/com/yl/string/MyStringPrac1.java
@@ -5,7 +5,7 @@ public class MyStringPrac1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str1 = "123456789";
-		System.out.println(str1.substring(3, 7) );
+		System.out.println(str1.substring(3, 7) );  // ×¢ÊÍ
 	}
 
 }
