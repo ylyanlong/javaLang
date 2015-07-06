781c65d93510ac4ff7fe8601a4edb98d868278f8
yanlong <ylyanlongm@gmail.com>
2015-07-07 01:25:26 +0800

    反射相关练习

---
+++ src/cn/com/yl/reflect/Breathe.java
@@ -0,0 +1,5 @@
+package cn.com.yl.reflect;
+
+public interface Breathe {
+	public void breathe();
+}
--- src/cn/com/yl/reflect/MyReflect.java
+++ src/cn/com/yl/reflect/MyReflect.java
@@ -1,5 +1,11 @@
 package cn.com.yl.reflect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import cn.com.yl.dp.*;
 
 public class MyReflect {
@@ -21,6 +27,186 @@ public class MyReflect {
 			e.printStackTrace();
 		} // 
 		
+		constructorObject("cn.com.yl.reflect.Person");
+		
+		showDeclaredMethods("cn.com.yl.reflect.Student");  // 并未出现 Object 相关的函数, Object 其实是二级父类, 可以测试下
+		showDeclaredMethods("cn.com.yl.reflect.Person"); 
+		
+		showDeclaredFields("cn.com.yl.reflect.Student");
+		
+		getAllPublicFields("cn.com.yl.reflect.Student");
+	}
+	
+	// classPath = "cn.com.yl.reflect.Person"
+	private static void constructorObject(String classPath){
+		try {
+			Class<?> classObject = Class.forName(classPath);
+			Constructor<?> constructObj = classObject.getConstructor(String.class);
+			// constructObj.setAccessible(true);  // 为 true 时取消 java 语言检查, 提升反射的速度
+			Object object = constructObj.newInstance("simple.test");
+			System.out.println("obj:  " + object.toString() );
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void showDeclaredMethods(String classPath){
+		try {
+			Class<?> classObj = Class.forName(classPath);
+			Method[] methods = classObj.getDeclaredMethods();
+			for(Method method: methods){
+				System.out.println(method.getName() );
+			}
+			
+			Method learnMetnod = classObj.getDeclaredMethod("learn", String.class);
+			Class<?>[] paramClass = learnMetnod.getParameterTypes();  // class 究竟是什么,感觉和泛型有很大关系
+			for(Class<?> classEle: paramClass){
+				System.out.println("paramType: " + classEle.getName() );
+			}
+			
+			System.out.println(learnMetnod.getName() + "  is private  " + Modifier.isPrivate(learnMetnod.getModifiers() ) );
+			
+			Student student = new Student("Sam");
+			learnMetnod.invoke(student, "java");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void getAllPublicMethod(String classPath){
+		try {
+			Class<?> classObj = Class.forName(classPath);
+			Method[] methods = classObj.getMethods();
+			for(Method methodEle: methods){
+				System.out.println("methodName: " + methodEle.getName() );
+			}
+			Method learnMethod = classObj.getMethod("learn", String.class);
+			System.out.println(learnMethod.getName() + "  is private  " + Modifier.isPrivate(learnMethod.getModifiers() ));
+			
+			Student student = new Student("Tom");
+			learnMethod.invoke(student, "test2");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void showDeclaredFields(String classPath){
+		try {
+			Class<?> classObj = Class.forName(classPath);
+			Field[] fields = classObj.getDeclaredFields();  // 获取到了所有字段,包括私有的
+			for(Field fieldEle: fields){
+				System.out.println("fieldName: " + fieldEle.getName() );
+			}
+			
+			Student student = new Student("Lily");
+			Field mGradeField = classObj.getDeclaredField("id");  // 只能获取共有字段
+			System.out.println("mGrade: " + mGradeField.getInt(student) );
+			mGradeField.setInt(student, 20);
+			System.out.println("mGrade: " + mGradeField.getInt(student) );
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	private static void getAllPublicFields(String classPath){
+		try {
+			Class<?> classObj = Class.forName(classPath);
+			Field[] fields = classObj.getFields();
+			for(Field fieldEle: fields){
+				System.out.println("field name: " + fieldEle.getName() );
+			}
+			
+			Student student = new Student("doggy");
+			Field mNameField = classObj.getField("id");
+			System.out.println("mNameField value: " + mNameField.getInt(student));
+			mNameField.setInt(student, 50);
+			System.out.println("mNameField value: " + mNameField.getInt(student));
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 }
